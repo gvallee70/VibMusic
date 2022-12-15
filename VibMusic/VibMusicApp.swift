471d75c23b13988fd3f396d94a337e265c4f757c
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2022-12-15 14:07:29 +0100

    Initial Commit

---
+++ VibMusic/VibMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibMusicApp.swift
+//  VibMusic
+//
+//  Created by Gwendal on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct VibMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
